51adcaefeff5d462df783749e8cdcd3b76eb2fe7
Mike <mbenko@benko.com>
2024-07-17 13:26:19 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# WGC24 - Journey to Well Governed Cloud
+
+Repo for code from the session on Journey to Well Governed Cloud
+
+## References
+
---
+++ scripts/setup.ps1
@@ -0,0 +1,11 @@
+git init
+
+git add .
+git commit -m "Initial commit"
+git branch -M main
+
+
+dotnet new webapp -o src/myApp
+dotnet new sln
+dotnet sln add src/myApp
+
